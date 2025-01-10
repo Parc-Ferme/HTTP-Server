b07ecdd33b065e0ef03f5bb34959cee9e2ee4f7c
potter-18 <ankityadaav13@gmail.com>
2025-01-10 19:53:03 +0530

    Send a response to client

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket  # noqa: F401
-
+import os
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +8,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address  = server_socket.accept() # wait for client
+
+    request = client_socket.recv(1024).decode('utf-8')  # Reading the HTTP request
+    print(f"Request received: {request}")
+
+
+    http_response = 'HTTP/1.1 200 OK \r\n\r\n'
+
+    client_socket.sendall(http_response.encode('utf-8'))
 
 
 if __name__ == "__main__":
